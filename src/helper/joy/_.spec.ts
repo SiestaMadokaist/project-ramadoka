83d5e7c4477ac8bc8c1c74f0629043a3435765e5
Rama Patria Himawan <siestamadokaist@gmail.com>
2020-02-26 11:45:20 +0700

    simple Joy/_.spec.ts

--- src/helper/joy/_.spec.ts
+++ src/helper/joy/_.spec.ts
@@ -3,6 +3,7 @@
  * it is all in typecheck.
  */
 import { Joy } from '.';
+import { logger } from '../logger';
 
 enum STATE {
   STARTED = 'STARTED',
@@ -10,32 +11,62 @@ enum STATE {
   COMPLETED = 'COMPLETED',
 }
 
-interface IA {
+interface MyInterface {
   arr_str: string[];
   obj_arr: Array<{
-    obj: {
-      obj: {};
+    x: {
+      y: {
+        z: number;
+      };
     };
   }>;
   obj: {
     obj: {
-      obj: {};
+      obj: {
+        obj: string;
+      };
     };
   };
 }
 
-const schema: Joy.SchemaOf<IA> = Joy.object<IA>().keys({
-  arr_str: Joy.array<IA['arr_str']>().items(Joy.string().required()).required(),
-  obj: Joy.object<IA['obj']>().keys({
-    obj: Joy.object<IA['obj']['obj']>().keys({
-      obj: Joy.object<IA['obj']['obj']['obj']>().keys({}).required(),
-    }).required(),
-  }).required(),
-  obj_arr: Joy.array<IA['obj_arr']>().items(Joy.object<IA['obj_arr'][0]>().keys({
-    obj: Joy.object<IA['obj_arr'][0]>().keys({
-      obj: Joy.object<IA['obj_arr'][0]['obj']>().keys({
-        obj: Joy.object<IA['obj_arr'][0]['obj']['obj']>().keys({}).required(),
+const mySchema: Joy.SchemaOf<MyInterface> = Joy.object<MyInterface>().keys({
+  arr_str: Joy.array<MyInterface['arr_str']>().items(Joy.string().required()).required(),
+  obj: Joy.object<MyInterface['obj']>().keys({
+    obj: Joy.object<MyInterface['obj']['obj']>().keys({
+      obj: Joy.object<MyInterface['obj']['obj']['obj']>().keys({
+        obj: Joy.string().required(),
       }).required(),
     }).required(),
-  }).required()).required(),
+  }).required(),
+  obj_arr: Joy.array<MyInterface['obj_arr']>().items(Joy.object<MyInterface['obj_arr'][0]>().required().keys({
+    x: Joy.object<MyInterface['obj_arr'][0]['x']>().required().keys({
+      y: Joy.object<MyInterface['obj_arr'][0]['x']['y']>().required().keys({
+        z: Joy.number().required(),
+      }),
+    }),
+  })).required(),
 }).required();
+
+describe('Joy', () => {
+  it('validate the schema', () => {
+    const myObject: MyInterface = {
+      arr_str: ['words'],
+      obj: {
+        obj: {
+          obj: {
+            obj: 'yes',
+          },
+        },
+      },
+      obj_arr: [{
+        x: {
+          y: {
+            z: 3,
+          },
+        },
+      }],
+    };
+    const result = mySchema.validate(myObject);
+    expect(result.value).toEqual(myObject);
+  });
+});
