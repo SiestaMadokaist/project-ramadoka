75d4a6c970839dbc2d2fe3ba3e6bc31513fc3969
Rama Patria Himawan <siestamadokaist@gmail.com>
2020-02-26 11:58:05 +0700

    added jest config

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "moduleFileExtensions": [ "ts", "tsx", "js" ],
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  "testMatch": [
+    "**/**/*.spec.(ts|tsx)"
+  ],
+  "testEnvironment": "node",
+  "collectCoverage": true,
+}
--- src/helper/joy/_.spec.ts
+++ src/helper/joy/_.spec.ts
@@ -13,6 +13,12 @@ enum STATE {
 
 interface MyInterface {
   arr_str: string[];
+  bool: boolean;
+  buffer: Buffer;
+  date: Date;
+  obj2: {
+    foo: 'bar',
+  };
   obj_arr: Array<{
     x: {
       y: {
@@ -31,12 +37,18 @@ interface MyInterface {
 
 const mySchema: Joy.SchemaOf<MyInterface> = Joy.object<MyInterface>().keys({
   arr_str: Joy.array<MyInterface['arr_str']>().items(Joy.string().required()).required(),
+  bool: Joy.boolean().required(),
+  buffer: Joy.binary().required(),
+  date: Joy.date().required(),
   obj: Joy.object<MyInterface['obj']>().keys({
     obj: Joy.object<MyInterface['obj']['obj']>().keys({
       obj: Joy.object<MyInterface['obj']['obj']['obj']>().keys({
         obj: Joy.string().required(),
       }).required(),
     }).required(),
+  }).required(),
+  obj2: Joy.object<MyInterface['obj2']>(true).keys({
+    foo: Joy.enums({ bar: 'bar' }).required(),
   }).required(),
   obj_arr: Joy.array<MyInterface['obj_arr']>().items(Joy.object<MyInterface['obj_arr'][0]>().required().keys({
     x: Joy.object<MyInterface['obj_arr'][0]['x']>().required().keys({
@@ -51,12 +63,18 @@ describe('Joy', () => {
   it('validate the schema', () => {
     const myObject: MyInterface = {
       arr_str: ['words'],
+      bool: true,
+      buffer: Buffer.from('0123', 'hex'),
+      date: new Date(),
       obj: {
         obj: {
           obj: {
             obj: 'yes',
           },
         },
+      },
+      obj2: {
+        foo: 'bar',
       },
       obj_arr: [{
         x: {
